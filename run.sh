f463e4894082aa34fc3668fe167e42c13f7af477
Dan Elbert <dan.elbert@gmail.com>
2015-01-02 15:56:30 -0600

    Changed auth

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8080:8080 --device=/dev/ttyZwave:/dev/ttyZwave0 -v /home/dan/Development/openhab-config:/opt/openhab/configurations danelbert/docker-openhab
+docker run -d -p 8080:8080 --device=/dev/ttyUSB1:/dev/ttyZwave0 -v /home/dan/Development/openhab-config:/opt/openhab/configurations danelbert/docker-openhab
