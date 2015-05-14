373e0b04f6027d780735b553563e340a60c0a71f
Dan Elbert <dan.elbert@gmail.com>
2015-05-13 21:51:14 -0500

    Updated test

--- run-test.sh
+++ run-test.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 6301:8080 -v /etc/openhab/configurations:/opt/openhab/configurations danelbert/docker-openhab:1.6.2
+docker run --rm -p 6301:8080 -v /home/dan/Development/openhab-config:/opt/openhab/configurations danelbert/docker-openhab
